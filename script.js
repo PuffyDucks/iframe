4849c7dc1a01b8692ceb147d5f62178e98b25a7d
cool9128 <9128cool@gmail.com>
2018-11-26 10:18:41 -0800

    X-mas

--- script.js
+++ script.js
@@ -1,12 +1,62 @@
 function onload() {
+
 }
 
 function setBrowserFrameSourceCheck(key){
-    if(key.keyCode === 13){
-        key.preventDefault();
+  if(key.keyCode === 13){
+    key.preventDefault();
+    var browserFrame = document.getElementById("browser");
+    if(document.getElementById("txtUrl").value === "gay"){
+      (function() {
+      var letter = "gay";
+      var color = "white";
+      var container = document.createElement('div');
+      document.body.appendChild(container);
+      container.style.position = 'fixed';
+      container.style.top = '0';
+      container.style.left = '0';
+      container.style.width = '100%';
+      container.style.height = '100%';
+      container.style.overflow = 'hidden';
+      container.style.pointerEvents = 'none';
 
-        setBrowserFrameSource();
+      function init() {
+          var injection = document.createElement('style');
+          document.body.appendChild(injection);
+          injection.innerHTML = '@-webkit-keyframes snow {\nfrom {top:-1%;}\nto{top:121%;}}\n@-moz-keyframes snow {\nfrom {top: -1%;}\nto {top: 121%;}\n}';
+      }
+      init();
+      window.setInterval(createParticle, 200);
+      timeChoices = [5, 6, 7, 8, 9, 10];
+
+      function destroy(evt) {
+          container.removeChild(evt.target);
+      }
+
+      function createParticle() {
+          var particle = document.createElement('span');
+          var randomSpeed = timeChoices[Math.floor(Math.random() * timeChoices.length)];
+          particle.innerHTML = letter;
+          particle.className = 'flake';
+          particle.style.position = 'absolute';
+          particle.style.color = color;
+          particle.style.backgroundColor = 'transparent';
+          particle.style.width = '5px';
+          particle.style.height = '5px';
+          particle.style.pointerEvents = 'none';
+          particle.style.right = Math.random() * 100 + '%';
+          particle.style.borderRadius = '50%';
+          particle.style.WebkitAnimation = 'snow ' + randomSpeed + 's linear';
+          particle.style.MozAnimation = 'snow ' + randomSpeed + 's linear';
+          container.appendChild(particle);
+          particle.addEventListener('webkitAnimationEnd', destroy);
+          particle.addEventListener('AnimationEnd', destroy);
     }
+})()
+    } else {
+      setBrowserFrameSource();
+    }
+  }
 }
 
 function setBrowserFrameSource(){
@@ -18,8 +68,7 @@ function setBrowserFrameSource(){
 }
 
 function setBrowserFrameSourceToBookmark1(){
-  var browserFrame = document.getElementById("browser");
-  browserFrame.src = "https://www.bing.com";
+  (function(){var d=document,s=d.createElement('script');if(d.getElementById('canvas_snow')){return;}s.src='http://www.wilmslowastro.com/html5/snow/snowfall.js';d.body.appendChild(s);})();
 }
 
 function setBrowserFrameSourceToBookmark2(){
