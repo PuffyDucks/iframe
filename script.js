2701061b2f96c035f4feaca7cdbed594539013c8
cool9128 <9128cool@gmail.com>
2018-12-17 14:41:09 -0800

    Error Page Test

--- script.js
+++ script.js
@@ -7,56 +7,72 @@ function setBrowserFrameSourceCheck(key){
     key.preventDefault();
     var browserFrame = document.getElementById("browser");
     if(document.getElementById("txtUrl").value === "gay"){
-      (function() {
-      var letter = "gay";
-      var color = "white";
-      var container = document.createElement('div');
-      document.body.appendChild(container);
-      container.style.position = 'fixed';
-      container.style.top = '0';
-      container.style.left = '0';
-      container.style.width = '100%';
-      container.style.height = '100%';
-      container.style.overflow = 'hidden';
-      container.style.pointerEvents = 'none';
-
-      function init() {
-          var injection = document.createElement('style');
-          document.body.appendChild(injection);
-          injection.innerHTML = '@-webkit-keyframes snow {\nfrom {top:-1%;}\nto{top:121%;}}\n@-moz-keyframes snow {\nfrom {top: -1%;}\nto {top: 121%;}\n}';
-      }
-      init();
-      window.setInterval(createParticle, 200);
-      timeChoices = [5, 6, 7, 8, 9, 10];
-
-      function destroy(evt) {
-          container.removeChild(evt.target);
-      }
-
-      function createParticle() {
-          var particle = document.createElement('span');
-          var randomSpeed = timeChoices[Math.floor(Math.random() * timeChoices.length)];
-          particle.innerHTML = letter;
-          particle.className = 'flake';
-          particle.style.position = 'absolute';
-          particle.style.color = color;
-          particle.style.backgroundColor = 'transparent';
-          particle.style.width = '5px';
-          particle.style.height = '5px';
-          particle.style.pointerEvents = 'none';
-          particle.style.right = Math.random() * 100 + '%';
-          particle.style.borderRadius = '50%';
-          particle.style.WebkitAnimation = 'snow ' + randomSpeed + 's linear';
-          particle.style.MozAnimation = 'snow ' + randomSpeed + 's linear';
-          container.appendChild(particle);
-          particle.addEventListener('webkitAnimationEnd', destroy);
-          particle.addEventListener('AnimationEnd', destroy);
-    }
-})()
+      snow("gay", "white")
+    } else if (document.getElementById("txtUrl").value === "Snow Halation") {
+      snow("Snow Halation", "white")
+    } else if (document.getElementById("txtUrl").value === "anime") {
+      snow("anime", "white")
+    } else if (document.getElementById("txtUrl").value === "google") {
+      snow("google", "white")
+    } else if (document.getElementById("txtUrl").value === "asdf") {
+      snow("asdf", "white")
+    } else if (document.getElementById("txtUrl").value === "pee") {
+      snow("pee", "yellow")
+    } else if (document.getElementById("txtUrl").value === "snow") {
+      snow("snow", "white")
+    } else if (document.getElementById("txtUrl").value === "egg") {
+      snow("egg", "white")
     } else {
       setBrowserFrameSource();
     }
   }
+}
+
+function snow(letter, color) {
+  (function() {
+  var container = document.createElement('div');
+  document.body.appendChild(container);
+  container.style.position = 'fixed';
+  container.style.top = '0';
+  container.style.left = '0';
+  container.style.width = '100%';
+  container.style.height = '100%';
+  container.style.overflow = 'hidden';
+  container.style.pointerEvents = 'none';
+
+  function init() {
+      var injection = document.createElement('style');
+      document.body.appendChild(injection);
+      injection.innerHTML = '@-webkit-keyframes snow {\nfrom {top:-1%;}\nto{top:121%;}}\n@-moz-keyframes snow {\nfrom {top: -1%;}\nto {top: 121%;}\n}';
+  }
+  init();
+  window.setInterval(createParticle, 200);
+  timeChoices = [5, 6, 7, 8, 9, 10];
+
+  function destroy(evt) {
+      container.removeChild(evt.target);
+  }
+
+  function createParticle() {
+      var particle = document.createElement('span');
+      var randomSpeed = timeChoices[Math.floor(Math.random() * timeChoices.length)];
+      particle.innerHTML = letter;
+      particle.className = 'flake';
+      particle.style.position = 'absolute';
+      particle.style.color = color;
+      particle.style.backgroundColor = 'transparent';
+      particle.style.width = '5px';
+      particle.style.height = '5px';
+      particle.style.pointerEvents = 'none';
+      particle.style.right = Math.random() * 100 + '%';
+      particle.style.borderRadius = '50%';
+      particle.style.WebkitAnimation = 'snow ' + randomSpeed + 's linear';
+      particle.style.MozAnimation = 'snow ' + randomSpeed + 's linear';
+      container.appendChild(particle);
+      particle.addEventListener('webkitAnimationEnd', destroy);
+      particle.addEventListener('AnimationEnd', destroy);
+    }
+  })()
 }
 
 function setBrowserFrameSource(){
