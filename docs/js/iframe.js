085988f1258722c39ff70c3e191eef88dd4942c7
PuffyDucks <PuffyDucks@campus-102-186.ucdavis.edu>
2023-04-19 00:16:13 -0700

    Local location update

--- docs/js/iframe.js
+++ docs/js/iframe.js
@@ -5,17 +5,29 @@ const bookmarks = [
   "https://slither.io",
   "https://secret.ethanl.ee/",
   "https://www.blankmediagames.com",
-  "info.html"
+  "home.html"
 ]
 
 function onload() {
   // Loads page from hash, or info page if none
   let iframe = document.getElementById("iframe");
-  if (location.hash === "") {
-    iframe.src = "info.html";
+  if (location.hash === "" || location.hash === "#home") {
+    iframe.src = "home.html";
+  } else if (location.hash === "#error") {
+    iframe.src = "error.html";
   } else {
     iframe.src = location.hash.substring(1);
   }
+  
+  iframe.onload = function() {
+    if (iframe.src === "https://puffyducks.github.io/iframe/home.html") {
+      location.hash = "home";
+    } else if (iframe.src === "https://puffyducks.github.io/iframe/error.html") {
+      location.hash = "error";
+    } else {
+      location.hash = iframe.src;
+    }
+  } 
 }
 
 // Checks if enter key is pressed on text field
@@ -26,9 +38,9 @@ function checkKeypress(key) {
   }
 }
 
-function setIFrameSource(){
+function setIFrameSource() {
   if (document.getElementById("urlbox").value === "") {
-    document.getElementById("iframe").src = "blank.html";
+    document.getElementById("iframe").src = "home.html";
   } else {
     document.getElementById("iframe").src = document.getElementById("urlbox").value;
   }
